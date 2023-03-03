66a2b65b01bc1123e1b51066a92208e6a7f63190
dafefo <115656165+dafefo@users.noreply.github.com>
2023-03-03 09:47:56 -0500

    Creacion de historgramas edades, nivel de colesterol y presion sanguinea en reposo

--- Analisis exploratorio.py
+++ Analisis exploratorio.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import pandas as pd
+import plotly.express as px
 
 #Apertura de los datos
 data_cardiaca = pd.read_csv("Analitica computacional/Proyecto1 Enfermedades cardiacas/cleveland_data.csv")
@@ -22,3 +23,65 @@ data_cardiaca["thal"]=pd.to_numeric(data_cardiaca["thal"])
 
 estadisticas = data_cardiaca.describe()
 
+#se crea una varaible categorica de 1 si esta diagnosticado con enfermedad cardiaca y 0 no
+data_cardiaca["cardiac"]=np.where(data_cardiaca["num"]>0,True,False)
+
+#Distribucion de las edades categorizando por sexo y si esta diagnosticado con enfermedad cardiaca
+fig = px.histogram(data_cardiaca,x = "age", color = "sex", pattern_shape = "cardiac",
+                   labels = {"age":"Age",
+                             "sex":"Sex",
+                             "cardiac":"Diagnostico"})
+fig.show()
+
+#Distribucion de los casos que reportan dolor en el pecho
+fig = px.pie(data_cardiaca, names = "cp")
+#fig.show()
+
+#Distribucion de la presion sanguinea en reposos sistolica
+fig = px.histogram(data_cardiaca,x = "trestbps", color = "sex", pattern_shape = "cardiac",
+                   labels = {"trestbps":"Presión sanguinea en reposo",
+                             "sex":"Sexo",
+                             "cardiac":"Diagnostico"})
+fig.add_annotation(x=120,
+            text="Valor normal",
+            showarrow=True,
+            arrowhead=1)
+fig.add_annotation(x=130,
+            text="Valor elevado",
+            showarrow=True,
+            arrowhead=1)
+fig.add_annotation(x=140,
+            text="Hipertension fase 1",
+            showarrow=True,
+            arrowhead=1)
+fig.add_annotation(x=180,
+            text="Hipertension fase 2",
+            showarrow=True,
+            arrowhead=1)
+fig.add_annotation(x=190,
+            text="Crisis de hipertension",
+            showarrow=True,
+            arrowhead=1)
+fig.show()
+
+#Distribucion de niveles de colesterol
+#Dada la grafiac se encontro que hay un dato atipico pues se tiene un colesterol mayor a 560 que esto ya estaria la persona muerta
+fig = px.histogram(data_cardiaca,x = "chol", color = "sex", pattern_shape = "cardiac",
+                   labels = {"chol":"nivel de colesterol",
+                             "sex":"Sexo",
+                             "cardiac":"Diagnostico"})
+fig.add_annotation(x=200,
+            text="Valor normal",
+            showarrow=True,
+            arrowhead=1)
+fig.add_annotation(x=240,
+            text="Nivel máximo",
+            showarrow=True,
+            arrowhead=1)
+fig.add_annotation(x=250,
+            text="Nivel alto",
+            showarrow=True,
+            arrowhead=1)
+fig.show()
+
+a = 1
