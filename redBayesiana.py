d95018f7b9e75b16f048a4130d5fba8931dca49b
cob91co <co.osorio90@uniandes.edu.co>
2023-03-08 10:43:28 -0500

    Se implemento el modelo bayesiano con las respectivas variables y los estimadores de maxima verosimilitud

--- redBayesiana.py
+++ redBayesiana.py
@@ -5,3 +5,66 @@ Created on Wed Mar  8 10:06:22 2023
 @author: baka
 """
 
+import numpy as np 
+import pandas as pd
+import plotly.express as px
+from pgmpy.models import BayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.estimators import MaximumLikelihoodEstimator
+from pgmpy.inference import VariableElimination
+##################CARGA Y MANIPULACION DE DATOS#####################################################
+#Apertura de los datos Daniel
+#data_cardiaca = pd.read_csv("Analitica computacional/Proyecto1 Enfermedades cardiacas/cleveland_data.csv")
+
+#Apertura datos Christer
+data_cardiaca = pd.read_csv("C:/Users/baka/Desktop/analitica/Proyectos/Proyecto_prediccion_enfermedades_cardiacas__actd/cleveland_data.csv")
+
+#En las variables ca y thal hay datos faltantes que se ubican con el simbolo ?
+print(data_cardiaca.loc[data_cardiaca["ca"]=="?"])
+#en la variable ca estan en las posiciones 166,192,287,302
+print(data_cardiaca.loc[data_cardiaca["thal"]=="?"])
+#en la variable Thal estan en 87 y 266
+#Es decir en total se deben eliminar 6 datos de la base de 303
+
+#Remover los datos faltantes
+faltantes = np.array([87,166,192,266,287,302])
+for i in faltantes:
+    data_cardiaca = data_cardiaca.drop(i)
+
+#Pasar las columans de tipo object a numeros
+data_cardiaca["ca"]=pd.to_numeric(data_cardiaca["ca"])
+data_cardiaca["thal"]=pd.to_numeric(data_cardiaca["thal"])
+
+estadisticas = data_cardiaca.describe()
+#Discretizar las variables
+#se crea una varaible categorica de 1 si esta diagnosticado con enfermedad cardiaca y 0 no
+data_cardiaca["cardiac"]=np.where(data_cardiaca["num"]>0,True,False)
+
+#Se crea una avraible categorica de la edad. 
+edad_discrt = pd.cut(data_cardiaca["age"],bins = [0,50,100], labels = ["Joven","Mayor"])
+data_cardiaca.insert(1,"age_group", edad_discrt)
+
+#Se crea una variable categorica para el colesterol
+chol_discrt = pd.cut(data_cardiaca["chol"],bins=[0,200,240,600], labels = ["normal","alto","muy alto"])
+data_cardiaca.insert(6,"chol_group",chol_discrt)
+
+#Se crea una varibale categorica para la presion sanguinea en reposo
+trestbps_discrt = pd.cut(data_cardiaca["trestbps"],bins=[0,119,129,139,179,210], labels=["normal","elevada","presion arterial nivel 1","presion arterial nivel 2","crisis"])
+data_cardiaca.insert(5,"trestbps_group", trestbps_discrt)
+print(data_cardiaca.head())
+print(data_cardiaca.columns)
+
+#########RED BAYESIANA################################################
+#Se crea el modelo Bayesiano
+model = BayesianNetwork([("sex", "cardiac"), ("fbs", "cardiac"), ("age_group","cardiac"), ("chol_group","cardiac"), ("cardiac", "exang"),("cardiac","slope"),("cardiac", "thal"), ("cardiac","cp"), ("cardiac","ca"), ("cardiac","trestbps_group"), ("cardiac","restecg")])
+emv = MaximumLikelihoodEstimator(model=model, data=data_cardiaca)
+
+model.fit(data=data_cardiaca, estimator = MaximumLikelihoodEstimator) 
+for i in model.nodes():
+    print(model.get_cpds(i))
+    
+model.check_model()
+
+infer = VariableElimination(model)
+posterior_p = infer.query(["cardiac"], evidence={"sex": 1, "age_group":"Mayor", "chol_group": "normal", "fbs": 1, "exang": 1})
+print(posterior_p)
